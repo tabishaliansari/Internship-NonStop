5f2b5c9bd753607354772e59fa30fd30b7b4de17
Tabish Ali Ansari <tabish.ansari@nonstopio.com>
2025-05-29 18:25:26 +0530

    Uploading Amigoscode project

---
+++ amigoscode/demo/demo/src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.demo.student;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+}
